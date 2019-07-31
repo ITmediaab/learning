b7e52e797362b50a7f1dbc0dcee47a8e5186d1c2
kid <adam_buczek@wp.pl>
2019-07-31 12:37:51 +0200

    "second commit"

--- src/pl/adam/com/App.java
+++ src/pl/adam/com/App.java
@@ -1,7 +1,11 @@
 package pl.adam.com;
 
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world");
+        JFrame ramka=new JFrame();
+
     }
 }
